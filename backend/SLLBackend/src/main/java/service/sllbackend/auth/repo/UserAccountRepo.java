c5d1c7e09b842b561c6067e4a7113b8ebe58f810
SonBui <sonbuilethai@gmail.com>
2025-09-29 16:19:11 +0700

    backend: basic form login

---
+++ backend/SLLBackend/src/main/java/service/sllbackend/auth/repo/UserAccountRepo.java
@@ -0,0 +1,8 @@
+package service.sllbackend.auth.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import service.sllbackend.auth.entity.UserAccount;
+
+public interface UserAccountRepo extends JpaRepository<UserAccount, Long> {
+    UserAccount findByUsername(String username);
+}

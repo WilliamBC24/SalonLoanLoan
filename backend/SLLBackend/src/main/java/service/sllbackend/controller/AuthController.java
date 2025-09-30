8e2ab9637c9d6485877058008bdda6ad96692664
Nguyễn Đại An <daiantom172003@gmail.com>
2025-09-30 06:11:17 +0000

    using default package

--- backend/SLLBackend/src/main/java/service/sllbackend/controller/AuthController.java
+++ backend/SLLBackend/src/main/java/service/sllbackend/controller/AuthController.java
@@ -1,4 +1,4 @@
-package service.sllbackend.auth.controller;
+package service.sllbackend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

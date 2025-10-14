02395696a5d79b1ce70fd8fa28e0166ae50fb4ae
Nguyễn Đại An <daiantom172003@gmail.com>
2025-10-14 07:48:29 +0000

    cleanup redundancy

--- backend/SLLBackend/src/main/java/service/sllbackend/web/mvc/HomeController.java
+++ backend/SLLBackend/src/main/java/service/sllbackend/web/mvc/HomeController.java
@@ -2,7 +2,6 @@ package service.sllbackend.web.mvc;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

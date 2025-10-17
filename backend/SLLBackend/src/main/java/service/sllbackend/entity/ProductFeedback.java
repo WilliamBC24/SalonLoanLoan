545054c87fb219eb407af5d7e7cd1eca840b9dbb
WilliamBC24 <sonbuilethai@gmail.com>
2025-10-17 10:56:48 +0700

    database: add product review support

---
+++ backend/SLLBackend/src/main/java/service/sllbackend/entity/ProductFeedback.java
@@ -0,0 +1,42 @@
+package service.sllbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product_feedback")
+public class ProductFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_account_id", nullable = false)
+    private UserAccount userAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Short rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @OneToMany(mappedBy = "productFeedback", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductFeedbackImage> images = new ArrayList<>();
+}
+
+
